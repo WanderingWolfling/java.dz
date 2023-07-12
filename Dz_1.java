2920e692f399ddd527d5a7f58c7b9fbf6ce50a45
Wolfy <vaencomat@gmail.com>
2023-07-12 15:49:17 +0300

    End homework N3

---
+++ Dz_1.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+
+        LinkedList<Integer> reversedList = reverseLinkedList(linkedList);
+        System.out.println("Reversed LinkedList: " + reversedList);
+    }
+
+    public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> linkedList) {
+        LinkedList<Integer> reversedList = new LinkedList<>();
+        for (int i = linkedList.size() - 1; i >= 0; i--) {
+            reversedList.add(linkedList.get(i));
+        }
+        return reversedList;
+    }
+}
